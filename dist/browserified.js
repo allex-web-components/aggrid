9bf1ff8a4f1131283060f2c6536bf910a9cdae27
Andrija Petrovic <andrija.hers@gmail.com>
2023-09-01 10:59:24 +0200

    Correction on EditableAgGridField

--- dist/browserified.js
+++ dist/browserified.js
@@ -1325,10 +1325,12 @@ function createGridFields (execlib, lR, mylib) {
   function EditableAgGridFieldElement (id, options) {
     EditableAgGridElement.call(this, id, options);
     FieldBaseMixin.call(this, options);
+    this.valid = null;
   }
   lib.inherit(EditableAgGridFieldElement, EditableAgGridElement);
   FieldBaseMixin.addMethods(EditableAgGridFieldElement); //now EditableAgGrid's set_data is stomped over
   EditableAgGridFieldElement.prototype.__cleanUp = function () {
+    this.valid = null;
     FieldBaseMixin.prototype.destroy.call(this);
     EditableAgGridElement.prototype.__cleanUp.call(this);
   };
@@ -1345,8 +1347,10 @@ function createGridFields (execlib, lR, mylib) {
   EditableAgGridFieldElement.prototype.set_rows = function (rows) {
     return EditableAgGridElement.prototype.set_data.call(this, rows);
   };
+  EditableAgGridFieldElement.prototype.get_value = function () {
+    return this.data;
+  }
   EditableAgGridFieldElement.prototype.isValueValid = function (val) {
-    console.log('EditableAgGridFieldElement isValueValid?', val);
     return true;
   };
   EditableAgGridFieldElement.prototype.resetData = function () {
